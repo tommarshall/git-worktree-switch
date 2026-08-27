385df9e5e559a92a97dd49f5deb8785dc3902212
Tom Marshall <tommarshall7@gmail.com>
2026-08-27 08:43:31 +0100

    Silence SC2329 on trap-invoked cleanup in test.sh
    * shellcheck's new SC2329 check flags `cleanup` as never
      invoked, but it runs via the `trap cleanup EXIT` below it.
    * Add SC2329 to the existing disable comment so `make lint`
      passes.

--- test.sh
+++ test.sh
@@ -74,7 +74,7 @@ menu_from() { # startdir args...
   wt "$@" <<< "" 2>/dev/null
 }
 
-# shellcheck disable=SC2317  # invoked indirectly via the EXIT trap below
+# shellcheck disable=SC2317,SC2329  # invoked indirectly via the EXIT trap below
 cleanup() { [ -n "${ROOT:-}" ] && rm -rf "$ROOT"; }
 trap cleanup EXIT
 
